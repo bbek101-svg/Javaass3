f1903da8e67c58f321328d6fe8cf4a40b61ee168
bbek101-svg <84989535+bbek101-svg@users.noreply.github.com>
2022-04-18 23:50:32 -0600

    Initial Creation

---
+++ src/exceptions/TreeException.java
@@ -0,0 +1,8 @@
+package exceptions;
+
+public class TreeException extends Exception{
+	
+	public TreeException(String message){
+        super(message);
+    }
+}
---
+++ test/BSTreeTest.java
@@ -0,0 +1,72 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class BSTreeTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testGetRoot() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetHeight() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSize() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testIsEmpty() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testClear() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testContains() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSearch() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAdd() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testInorderIterator() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testPreorderIterator() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testPostorderIterator() {
+		fail("Not yet implemented");
+	}
+
+}
