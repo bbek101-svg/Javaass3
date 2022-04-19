98f4f22f52bc0805fc67e76ba9716afda19d7ff8
bbek101-svg <84989535+bbek101-svg@users.noreply.github.com>
2022-04-19 00:06:35 -0600

    Updated BStreeNode

--- src/referenceBasedTreeImplementation/BSTreeNode.java
+++ src/referenceBasedTreeImplementation/BSTreeNode.java
@@ -1,32 +1,89 @@
 package referenceBasedTreeImplementation;
 
 public class BSTreeNode<E> {
-	private E element;
-	private BSTreeNode<E> left, right;
+
+	private E data;
+	private BSTreeNode left;
+	private BSTreeNode right;
+
 	
+	public BSTreeNode(E elem) {
+		this.data = elem;
+		this.left = null;
+		this.right = null;
+	}
+
 	public BSTreeNode(E elem, BSTreeNode<E> left, BSTreeNode<E> right) {
-		this.element = element;
+		this.data = elem;
 		this.left = left;
 		this.right = right;
 	}
-	
-	public BSTreeNode(E elem) {
-		this.element = elem;
+
+	public int getHeight() {
+
+		return getHeight(this);
 	}
-	
+
+	private int getHeight(BSTreeNode<E> node) {
+		if (node == null)
+			return 0;
+
+		int leftHeight = getHeight(node.left);
+		int rightHeight = getHeight(node.right);
+
+		if (leftHeight > rightHeight) {
+			return leftHeight + 1;
+		} else {
+			return rightHeight + 1;
+		}
+	}
+
 	public E getElement() {
-		return this.element;
+		return this.data;
 	}
-	
+
 	public void setElement(E elem) {
-		this.element = elem;
+		this.data = elem;
 	}
-	
-	public BSTreeNode<E> getLeft(){
+
+	public BSTreeNode<E> getLeft() {
 		return this.left;
 	}
-	
-	public BSTreeNode<E> getRight(){
+
+	public void setLeft(BSTreeNode<E> left) {
+		this.left = left;
+	}
+
+	public BSTreeNode<E> getRight() {
 		return this.right;
 	}
+
+	public void setRight(BSTreeNode<E> right) {
+		this.right = right;
+	}
+
+	public boolean hasLeftChild() {
+		return (this.left != null);
+	}
+
+	public boolean hasRightChild() {
+		return (this.right != null);
+	}
+
+	public boolean isLeaf() {
+		return (this.left == null && this.right == null);
+	}
+
+	public int getNumberNodes() {
+
+		return getNumberNodes(this);
+	}
+
+	private int getNumberNodes(BSTreeNode<E> node) {
+		if (node == null)
+			return 0;
+
+		return getNumberNodes(node.left) + getNumberNodes(node.right) + 1;
+	}
+
 }
